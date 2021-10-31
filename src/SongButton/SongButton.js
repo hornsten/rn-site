e17be4b85cfc68da51991ce80b9337a58571f65f
Jessica Hornsten <jhornsten@comcast.net>
2021-10-31 16:21:04 -0500

    set buttons to absolute position, fixed animation

--- src/SongButton/SongButton.js
+++ src/SongButton/SongButton.js
@@ -9,23 +9,23 @@ function SongButton(props) {
   function handleClick(e) {
       setIsToggled(!isToggled);
   }
-  const brainClipPath = 'polygon(63.04% 9.27%, 76.9% 19.33%, 90.91% 29.06%, 98.52% 41.27%, 100.22% 57.75%, 99.05% 75.1%, 91.61% 96.09%, 75.19% 104.41%, 45.34% 90.16%, 35.56% 82.46%, 30.34% 69.56%, 21.53% 63.09%, 10.98% 59.17%, 4.94% 56.27%, 1.02% 47.28%, 4.48% 30.57%, 11.81% 14.8%, 26.4% 4.12%, 47.08% 2.29%)';
+  const brainClipPath = 'polygon(67.29% 7.59%, 80.4% 19.67%, 88.57% 34.12%, 92.49% 46.48%, 93.22% 62.53%, 90.97% 73.8%, 82.03% 92.86%, 68.44% 99.38%, 55.71% 96.70%, 43.84% 94.19%, 33.06% 85.81%, 28.34% 72.91%, 19.53% 62.42%, 6.48% 59.84%, 1.94% 51.91%, 5.02% 36.21%, 13.98% 18.49%, 25.82% 8.09%, 40.16% 3.11%, 55.58% 2.96%';
 
   const fade = useSpring({
-      config: config.molasses,
-      zIndex: isToggled ? 100 : 'revert',
-      clipPath: isToggled ?  brainClipPath : props.clipPath || 'unset',
-      height: isToggled ? '43vh' : props.height || '10vh',
-      width: isToggled ? '53vh' : props.width || '13vh',
-      position: isToggled ? 'absolute' : 'relative',
-      backgroundSize: isToggled ? 'cover' : 'contain',
-      backgroundImage: props.backgroundImage ? props.backgroundImage : 'none',
-      backgroundColor: props.backgroundImage ? 'none' : 'tomato',
-      opacity: isToggled ? '1' : '0.5'
+      config: config.slow,
+      immediate: isToggled ? key => key === 'zIndex' : key => key === '',
+      zIndex: isToggled ? '12' : '1',
+      clipPath: isToggled ?  brainClipPath : props.clipPath,
+      height: isToggled ? '100%' : props.height,
+      width: isToggled ? '100%' : props.width,
+      position: isToggled ? 'relative' : 'absolute',
+      top: isToggled ? '0%' : props.top,
+      right: isToggled ? '0%' : props.right,
+      color: isToggled ? '#fff' : 'transparent'
     });
     return (
         <>
-        <animated.button className={isToggled? "item toggled" : "item arrowBg"} style={fade} onClick={handleClick}>
+        <animated.button id={props.id} className={isToggled? "item toggled" : "item"} style={fade} onClick={handleClick}>
             {props.name}
         </animated.button>
         </>
